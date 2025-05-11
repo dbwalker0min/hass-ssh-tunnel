fb022942770b8bb6ab6384cf51e45bf866a02f81
David Walker <dbwalker0min@gmail.com>
2025-05-11 16:28:25 -0700

    Should start working...

--- run.sh
+++ run.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-SSH_USER="${1:-your_ssh_user}"
-SSH_HOST="${2:-your.public.server.com}"
-SSH_PKEY="${3:-/path/to/your/private/key}"
-SSH_PORT="${4:-8123}"
+# Ensure ~/.ssh exists and is linked to the config folder version
+mkdir -p /root/.ssh
+cp -r /config/.ssh/* /root/.ssh/
+chmod 600 /root/.ssh/*
+
+# Look at all the options
+cat /data/options.json
+
+SSH_HOST=$(jq -r '.ssh_host' /data/options.json)
+REMOTE_PORT=$(jq -r '.remote_port' /data/options.json)
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') Tunnel starting to $SSH_HOST (remote port: $REMOTE_PORT → local port: 8123)"
 
 while true; do
-    autossh -M 0 -N -R ${REMOTE_PORT}:localhost:${LOCAL_PORT} ${SSH_USER}@${SSH_HOST} -p ${SSH_PORT}
+    autossh -M 0 -N -R ${REMOTE_PORT}:localhost:8123 ${SSH_HOST} 2>&1
+    echo "$(date '+%Y-%m-%d %H:%M:%S') Tunnel disconnected; retrying..."
     sleep 10
 done
