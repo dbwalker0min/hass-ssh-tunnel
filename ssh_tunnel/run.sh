190a173410bd8ba949440513da5316a46cf46af3
David Walker <dbwalker0min@gmail.com>
2025-05-12 23:17:41 +0000

    Rename service with a dash

--- ssh_tunnel/run.sh
+++ ssh_tunnel/run.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/with-contenv bash
 # shellcheck shell=bash
 
-# Ensure ~/.ssh exists and is linked to the config folder version
-mkdir -p /root/.ssh
-cp -r /config/.ssh/* /root/.ssh/
-chmod 600 /root/.ssh/*
+echo "Starting SSH tunnel..."
 
 # Look at all the options
 cat /data/options.json
+
+# Ensure ~/.ssh exists and is linked to the config folder version
+mkdir -p /root/.ssh
+cp -r /config/.ssh/* /root/.ssh/
+chown -R root:root /root/.ssh
+chmod 600 /root/.ssh/* 2>/dev/null
+chmod 644 /root/.ssh/*.pub 2>/dev/null
+
 
 SSH_HOST=$(bashio::config 'ssh_host')
 REMOTE_PORT=$(bashio::config 'remote_port' 8123)
