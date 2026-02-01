d68a541543275a8b8bd525736568a387fe9d72ba
David Walker <dbwalker0min@gmail.com>
2026-02-01 12:28:15 -0800

    Added ports for pyscript jupyter shim

--- ssh_tunnel/run.sh
+++ ssh_tunnel/run.sh
@@ -4,9 +4,8 @@
 echo "Starting SSH tunnel..."
 
 SSH_HOST=$(bashio::config 'ssh_host')
-REMOTE_PORT=$(bashio::config 'remote_port' 8123)
-LOCAL_HOST=$(bashio::config 'local_host' 'localhost')
-LOCAL_PORT=$(bashio::config 'local_port' 8123)
+LOCAL_FWD_HOST="127.0.0.1"
+REMOTE_BIND_HOST="127.0.0.1"
 SSH_USER=$(bashio::config 'ssh_user' 'homeassistant')
 OTHER_SSH_OPTIONS=$(bashio::config 'other_ssh_options' '')
 SSH_KEY_PATH=$(bashio::config 'ssh_key_path')
@@ -18,7 +17,7 @@ else
     exit 1
 fi
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') Tunnel starting to $SSH_HOST (remote port: $REMOTE_PORT → $LOCAL_HOST:$LOCAL_PORT) as $SSH_USER"
+echo "$(date '+%Y-%m-%d %H:%M:%S') Tunnel starting to $SSH_HOST as $SSH_USER"
 
 # Make sure the .ssh directory exists and has the correct permissions
 mkdir -p /root/.ssh
@@ -38,11 +37,37 @@ while true; do
     chmod 600 /root/.ssh/known_hosts
 
     echo "Starting autossh with ${OTHER_SSH_OPTIONS}"
-    eval autossh -N -R ${REMOTE_PORT}:${LOCAL_HOST}:${LOCAL_PORT} \
+
+    # This starts the autossh command forwarding the home assistant port and the pyscript kernel ports
+    autossh -N \
+        -R ${REMOTE_BIND_HOST}:8123:${LOCAL_FWD_HOST}:8123 \
+        -R ${REMOTE_BIND_HOST}:50321:${LOCAL_FWD_HOST}:50321 \
+        -R ${REMOTE_BIND_HOST}:50322:${LOCAL_FWD_HOST}:50322 \
+        -R ${REMOTE_BIND_HOST}:50323:${LOCAL_FWD_HOST}:50323 \
+        -R ${REMOTE_BIND_HOST}:50324:${LOCAL_FWD_HOST}:50324 \
+        -R ${REMOTE_BIND_HOST}:50325:${LOCAL_FWD_HOST}:50325 \
+        -R ${REMOTE_BIND_HOST}:50326:${LOCAL_FWD_HOST}:50326 \
+        -R ${REMOTE_BIND_HOST}:50327:${LOCAL_FWD_HOST}:50327 \
+        -R ${REMOTE_BIND_HOST}:50328:${LOCAL_FWD_HOST}:50328 \
+        -R ${REMOTE_BIND_HOST}:50329:${LOCAL_FWD_HOST}:50329 \
+        -R ${REMOTE_BIND_HOST}:50330:${LOCAL_FWD_HOST}:50330 \
+        -R ${REMOTE_BIND_HOST}:50331:${LOCAL_FWD_HOST}:50331 \
+        -R ${REMOTE_BIND_HOST}:50332:${LOCAL_FWD_HOST}:50332 \
+        -R ${REMOTE_BIND_HOST}:50333:${LOCAL_FWD_HOST}:50333 \
+        -R ${REMOTE_BIND_HOST}:50334:${LOCAL_FWD_HOST}:50334 \
+        -R ${REMOTE_BIND_HOST}:50335:${LOCAL_FWD_HOST}:50335 \
+        -R ${REMOTE_BIND_HOST}:50336:${LOCAL_FWD_HOST}:50336 \
+        -R ${REMOTE_BIND_HOST}:50337:${LOCAL_FWD_HOST}:50337 \
+        -R ${REMOTE_BIND_HOST}:50338:${LOCAL_FWD_HOST}:50338 \
+        -R ${REMOTE_BIND_HOST}:50339:${LOCAL_FWD_HOST}:50339 \
         -n \
-        -i ${SSH_KEY_PATH} \
+        -i "${SSH_KEY_PATH}" \
+        -M 20000 \
+        -o ServerAliveInterval=60 \
+        -o ServerAliveCountMax=5 \
+        -o ExitOnForwardFailure=yes \
         ${OTHER_SSH_OPTIONS} \
-        ${SSH_USER}@${SSH_HOST} 2>&1
+        "${SSH_USER}@${SSH_HOST}" 2>&1
     EXIT_CODE=$?
     echo "$(date '+%Y-%m-%d %H:%M:%S') Tunnel disconnected; retrying..."
 
